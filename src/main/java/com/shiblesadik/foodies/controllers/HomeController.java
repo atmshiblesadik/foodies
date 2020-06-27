b9f76da62c7e7d9c978db967d1cc08aea83695f0
atmshiblesadik <shiblesadik@outlook.com>
2020-06-27 13:27:19 +0600

    index view initialize

---
+++ src/main/java/com/shiblesadik/foodies/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.shiblesadik.foodies.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Foodies | Home</title>
+</head>
+<body>
+<h1>Welcome Foodies</h1>
+<h2>Welcome Foodies</h2>
+<h3>Welcome Foodies</h3>
+<h4>Welcome Foodies</h4>
+</body>
+</html>
