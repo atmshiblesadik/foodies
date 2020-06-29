97be9cde1aa723a099082bb9da4064620d27aad5
atmshiblesadik <shiblesadik@outlook.com>
2020-06-30 00:02:31 +0600

    login design

--- src/main/java/com/shiblesadik/foodies/controllers/customer/CustomerController.java
+++ src/main/java/com/shiblesadik/foodies/controllers/customer/CustomerController.java
@@ -3,6 +3,7 @@ package com.shiblesadik.foodies.controllers.customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,12 +13,29 @@ public class CustomerController {
     @GetMapping("/registration")
     public String getRegistration(Model model) {
         model.addAttribute("title", "Customer Registration");
+        model.addAttribute("action", "/customer/registration");
+        model.addAttribute("login", "/customer/login");
         return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String postRegistration(Model model) {
+        model.addAttribute("title", "Customer Registration");
+        return "index";
     }
 
     @GetMapping("/login")
     public String getLogin(Model model) {
         model.addAttribute("title", "Customer Login");
+        model.addAttribute("action", "/customer/login");
+        model.addAttribute("forgotPass", "/customer/forgotpass");
+        model.addAttribute("registration", "/customer/registration");
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String postLogin(Model model) {
+        model.addAttribute("title", "Customer Login");
+        return "index";
     }
 }
