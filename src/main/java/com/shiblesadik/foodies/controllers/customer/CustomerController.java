c7e1cf1d1a06ace6569a96435ed9357415a44ba7
atmshiblesadik <shiblesadik@outlook.com>
2020-07-01 00:19:23 +0600

    getting data from form submission

--- src/main/java/com/shiblesadik/foodies/controllers/customer/CustomerController.java
+++ src/main/java/com/shiblesadik/foodies/controllers/customer/CustomerController.java
@@ -1,8 +1,10 @@
 package com.shiblesadik.foodies.controllers.customer;
 
+import com.shiblesadik.foodies.services.RegistrationValidation;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +14,7 @@ public class CustomerController {
 
     @GetMapping("/registration")
     public String getRegistration(Model model) {
+        model.addAttribute("registrationValidation", new RegistrationValidation());
         model.addAttribute("title", "Customer Registration");
         model.addAttribute("action", "/customer/registration");
         model.addAttribute("login", "/customer/login");
@@ -19,8 +22,11 @@ public class CustomerController {
     }
 
     @PostMapping("/registration")
-    public String postRegistration(Model model) {
-        model.addAttribute("title", "Customer Registration");
+    public String postRegistration(@ModelAttribute RegistrationValidation registrationValidation) {
+        System.out.println("username: " + registrationValidation.getUsername());
+        System.out.println("email: " + registrationValidation.getEmail());
+        System.out.println("password: " + registrationValidation.getPassword());
+        System.out.println("confirm password: " + registrationValidation.getConfirmPassword());
         return "index";
     }
 
